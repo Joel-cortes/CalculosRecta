971d16c0a3678c84d56aa0972ee8c09b19823d32
juan joel cortes montes <jotamontes678@gmail.com>
2025-08-13 17:25:47 -0500

    codigo completo

--- src/App.java
+++ src/App.java
@@ -2,28 +2,37 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Programa para realizar calculos de la ecuacion lineal");
+        System.out.println("Programa para realizar calculos de la Ecuacion Lineal ");
 
-        // declarar objeto para leer datos por consola
-        Scanner lector=new Scanner(System.in); 
+        //declarar objeto
+        Scanner lector = new Scanner(System.in);
 
-        // leer los datos de entrada
-        System.out.println("Coordenadas del primer punto");
-        System.out.print("x ?");
-        double x1=lector.nextDouble();
-        System.out.print("y ?");
-        double y1=lector.nextDouble();
+        //leer datos
+        System.out.println("Coordenadas del primer punto ");
+        System.out.print("x? ");
+        double x1 = lector.nextDouble();
+        System.out.print("y? ");
+        double y1 = lector.nextDouble();
 
-        System.out.println("Coordenadas del segundo punto");
-        System.out.print("x ?");
-        double x2=lector.nextDouble();
-        System.out.print("y ?");
-        double y2=lector.nextDouble();
+        System.out.println("Coordenadas del primer punto ");
+        System.out.print("x? ");
+        double x2 = lector.nextDouble();
+        System.out.print("y? ");
+        double y2 = lector.nextDouble();
 
-        // Proceso
-        double distancia=Math.sqrt(Math.pow(y2-y1,2)+Math.pow(x2-x1,2));
+        //proceso
+        double distancia=Math.sqrt(Math.pow(y2-y1,2) + Math.pow(x2-x1,2));
+        double pendiente=(y2-y1)/(x2-x1);
+        double b = y1-(pendiente*x1);
 
-        // Salida
-        System.out.println("La distancia entre los puntos es: " + distancia);
+        //salida
+        System.out.println("La distancia entre los puntos es " + distancia);
+        if(pendiente<0)
+        {
+            System.out.println("y = "+pendiente+"x "+b);
+        } else {
+            System.out.println("y = "+pendiente+"x +"+b);
+        }
+
     }
 }
